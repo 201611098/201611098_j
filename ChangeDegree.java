1cac3152388e5866924a28108dbd73d34e36d117
201611098 <heein4653@hanmail.net>
2016-11-01 21:33:23 +0900

    Update ChangeDegree.java

--- ChangeDegree.java
+++ ChangeDegree.java
@@ -4,23 +4,31 @@ class ChangeDegree{
   public void setDegree(float d){
     float degree=d;
   }
-  public void setString(String s);
+  public void setName(String s){
     String name=s;
+  }
+  public void getDegree(){
+    return degree;
+  }
+  public void getName(){
+    return name;
   }
   public void CtoF(float d){
     this.d=(d*1.8)+3.2;
+    System.out.println(float d);
   }
   public void FtoC(float d){
     this.d=(d-32)/1.8;
+    System.out.println(float d);
   }
-  public void change(float d){
+  public void change(float d,String s){
     if(s.equals("C")){
       CtoF(float d);
-    } 
-  }
-  public void change(float d){
-    if(s.equals("F")){
+    elseif(s.equals("F")){
       FtoC(float d);
     } 
+    else{
+      default;
+    }
   }
-}      
+}    
