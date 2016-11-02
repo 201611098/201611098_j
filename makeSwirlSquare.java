f3a4d2537bef1ca3edfac901e2e1449b316737f7
201611098 <heein4653@hanmail.net>
2016-11-02 14:30:20 +0900

    Update makeSwirlSquare.java

--- makeSwirlSquare.java
+++ makeSwirlSquare.java
@@ -1,9 +1,19 @@
-void makeSwirlSquare(int size,int bigger,int turns,double degree)
-for(int i=0; i<turns; i++){
-  if(i%2!=0){
-    size+=bigger;
-  }
-  t1.forward(size);
-  t1.right(degree);
+import ch.aplu.turtle.*;
+
+public class MakeSwirlSquare{
+  static Turtle t1=new Turtle();
+
+  public void makeSwirlSquare(int size,int bigger,int turns,double degree){
+    for(int i=0; i<turns; i++){
+      if(i%2!=0){
+         size+=bigger;
+      }
+      t1.forward(size);
+      t1.right(degree);
   //System.out.printf("%d-%d\n",i,size);
+    }
+    public static void main(String[] args){
+      makeSwirlSquare(30,30,30,90.0);
+    }   
+  }
 }
